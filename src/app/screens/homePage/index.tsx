ebd8011b8e28c688c6c08b9905a9725c033ad8e4
qobil17 <ibrohimovqobilbek5@gmail.com>
2025-05-15 13:49:36 +0900

    fix: retrieve fresh menu and modify NewDishes component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,22 +5,22 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
-
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import "../../../css/home.css";
 
 /** REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 });
 
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatch(useDispatch()); // reduxga borib slicedagi metodlarni malumotni bog'lashni aytadi
+  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch()); // reduxga borib slicedagi metodlarni malumotni bog'lashni aytadi
  //usedispach store bilan bog'lab beradi
   // Selector: Store => Data
   console.log(process.env.REACT_APP_API_URL);
@@ -35,6 +35,15 @@ export default function HomePage() {
     }).then(data => {
       console.log("data:", data);
       setPopularDishes(data);
+    }).catch(err => console.log(err));
+
+     product.getProducts({
+      page: 1,
+      limit: 5,
+      order: "createdAt",
+     // productCollection: ProductCollection.DISH,
+     }).then(data => {
+       setNewDishes(data);
     }).catch(err => console.log(err));
 
   }, []);
